4f8ad4fd52d11a92760393dca9bb87cf44dad0ac
Calvin Roberts <roberts.calvin85@gmail.com>
2023-05-31 01:05:51 +0100

    Get list of 100 movies in order

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/m
 
 # Write your code below this line 👇
 
+response = requests.get(URL)
+website_html = response.text
 
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all("h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+print(movie_titles[::-1])
